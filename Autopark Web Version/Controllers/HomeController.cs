c83bae985fdb089a707de2330a8452b49aa5b33b
Kirill <kirill.shlemen@gmail.com>
2021-01-13 00:30:41 +0300

    add VenicleController

--- Autopark Web Version/Controllers/HomeController.cs
+++ Autopark Web Version/Controllers/HomeController.cs
@@ -5,58 +5,12 @@ using System.Diagnostics;
 namespace Autopark_Web_Version.Controllers
 {
     public class HomeController : Controller
-    {        
-        IRepository<Venicles> repo;
-        public HomeController(IRepository<Venicles> r)
-        {
-            repo = r;
-        }        
+    {
 
-        public ActionResult Index()
-        {
-            return View(repo.GetAll());
-        }
-        public ActionResult Create()
+        public IActionResult Index()
         {
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult Create(Venicles venicles)
-        {
-            repo.Create(venicles);
-            return RedirectToAction("Index");
-        }
-        public ActionResult Edit(int id)
-        {
-            Venicles venicles = repo.Get(id);
-            if (venicles != null)
-                return View(venicles);
-            return NotFound();
-        }
-
-        [HttpPost]
-        public ActionResult Edit(Venicles venicles)
-        {
-            repo.Update(venicles);
-            return RedirectToAction("Index");
-        }
-
-        [HttpGet]
-        [ActionName("Delete")]
-        public ActionResult ConfirmDelete(int id)
-        {
-            Venicles venicles = repo.Get(id);
-            if (venicles != null)
-                return View(venicles);
-            return NotFound();
-        }
-        [HttpPost]
-        public ActionResult Delete(int id)
-        {
-            repo.Delete(id);
-            return RedirectToAction("Index");
-        }
+        }        
         public ActionResult Privacy()
         {
             return View();
