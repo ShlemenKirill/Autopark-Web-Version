28547ff1431fe0fbb308dac1c6f46f49dc945f9f
Kirill <kirill.shlemen@gmail.com>
2021-01-13 00:08:13 +0300

    modify repositories, add dispose pattern

--- Autopark Web Version/Models/Repositories/VenicleTypeRepository.cs
+++ Autopark Web Version/Models/Repositories/VenicleTypeRepository.cs
@@ -9,54 +9,72 @@ using System.Threading.Tasks;
 namespace Autopark_Web_Version.Models.Repositories
 {
     public class VenicleTypeRepository : IRepository<VenicleType>
-    {
-        string connectionString = null;
-        public VenicleTypeRepository(string conn)
+    {        
+        readonly IDbConnection connection = null;
+        public VenicleTypeRepository(string dbConnection)
         {
-            connectionString = conn;
+            connection = new SqlConnection(dbConnection);
         }
         public List<VenicleType> GetAll()
-        {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                return db.Query<VenicleType>("SELECT * FROM VenicleType").ToList();
-            }
+        {            
+            return connection.Query<VenicleType>("SELECT * FROM VenicleType").ToList();
         }
         public void Create(VenicleType entity)
         {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                var sqlQuery = "INSERT INTO Venicles (Engine, ModelName,RegistrationNumber, Weight, Year, Color, Mileage,Tank) " +
-                    "VALUES(@Engine, @ModelName, @RegistrationNumber, @Weight, @Year, @Color, @Mileage, @Tank)";
-                db.Execute(sqlQuery, entity);
-            }
+            
+            var sqlQuery = "INSERT INTO Venicles (Engine, ModelName,RegistrationNumber, Weight, Year, Color, Mileage,Tank) " +
+                "VALUES(@Engine, @ModelName, @RegistrationNumber, @Weight, @Year, @Color, @Mileage, @Tank)";
+            connection.Execute(sqlQuery, entity);
         }
 
         public void Delete(int id)
-        {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                var sqlQuery = "DELETE FROM Venicles WHERE Id = @id";
-                db.Execute(sqlQuery, new { id });
-            }
+        {            
+            var sqlQuery = "DELETE FROM Venicles WHERE Id = @id";
+            connection.Execute(sqlQuery, new { id });
         }
 
         public VenicleType Get(int id)
-        {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                return db.Query<VenicleType>("SELECT * FROM Venicles WHERE Id = @id", new { id }).FirstOrDefault();
-            }
+        {            
+            return connection.Query<VenicleType>("SELECT * FROM Venicles WHERE Id = @id", new { id }).FirstOrDefault();
         }
 
         public void Update(VenicleType entity)
-        {
-            using (IDbConnection db = new SqlConnection(connectionString))
-            {
-                var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
-                db.Execute(sqlQuery, entity);
-            }
+        {            
+            var sqlQuery = "UPDATE Users SET Name = @Name, Age = @Age WHERE Id = @Id";
+            connection.Execute(sqlQuery, entity);
         }
+
+        #region Disposable
+
+        private bool _disposedValue;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposedValue)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                connection.Dispose();
+            }
+
+            _disposedValue = true;
+        }
+
+        ~VenicleTypeRepository()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion Disposable
 
     }
 }
