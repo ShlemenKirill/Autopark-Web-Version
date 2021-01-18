904e315a8ba55395c0d2e0ed02a7c82e9c29a696
Kirill <kirill.shlemen@gmail.com>
2021-01-18 22:34:16 +0300

    add IDetailsRepository

---
+++ Autopark Web Version/Models/Interfaces/IDetailsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autopark_Web_Version.Models.Interfaces
+{
+    interface IDetailsRepository
+    {
+        void Create(Details entity);
+        void Delete(int id);
+        Details Get(int id);
+        List<Details> GetAll();
+        void Update(Details entity);        
+    }
+}
