5efaefee66b452715595559e77bd0557132ed3a1
Kirill <kirill.shlemen@gmail.com>
2021-01-18 22:42:28 +0300

    create Details repository

--- Autopark Web Version/Models/Repositories/VenicleRepository.cs
+++ Autopark Web Version/Models/Repositories/VenicleRepository.cs
@@ -11,7 +11,7 @@ using System.Collections;
 
 namespace Autopark_Web_Version.Models.Repositories
 {
-    public class VenicleRepository : IVenicleRepository<Venicles>
+    public class VenicleRepository : IVenicleRepository<Venicles>, IDisposable
     {
         readonly IDbConnection connection = null;
         public VenicleRepository(string dbConnection)
