4b78311951cffbf6e388878ec6a6206af20d2f2b
Kirill <kirill.shlemen@gmail.com>
2021-01-18 22:31:49 +0300

    add details model

---
+++ Autopark Web Version/Models/Details.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autopark_Web_Version.Models
+{
+    public class Details
+    {
+        public int DetailId { get; set; }
+        public string DetailName { get; set; }
+    }
+}
