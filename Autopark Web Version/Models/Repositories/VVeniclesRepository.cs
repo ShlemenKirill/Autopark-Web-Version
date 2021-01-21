a2e7ed92ac8a7c86027f71b2607b2a6226b0ac76
Kirill <kirill.shlemen@gmail.com>
2021-01-21 20:30:46 +0300

    add repository

---
+++ Autopark Web Version/Models/Repositories/VVeniclesRepository.cs
@@ -0,0 +1,41 @@
+﻿using Autopark_Web_Version.Models.Interfaces;
+using Autopark_Web_Version.Models.Models;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autopark_Web_Version.Models.Repositories
+{
+    public class VVeniclesRepository : IReadOnlyRepository<VVEnicles>
+    {
+        readonly IDbConnection connection = null;
+        public VVeniclesRepository(string dbConnection)
+        {
+            connection = new SqlConnection(dbConnection);
+        }
+        public IEnumerable<VVEnicles> GetAll()
+        {
+            return connection.Query<VVEnicles>
+                (
+                "SELECT " +
+                "Venicles.VenicleId, " +
+                "VenicleType.VenicleType, " +
+                "Venicles.Engine, " +
+                "Venicles.ModelName, " +
+                "Venicles.RegistrationNumber, " +
+                "Venicles.Weight, " +
+                "Venicles.Year, " +
+                "Venicles.Color, " +
+                "Venicles.Mileage, " +
+                "Venicles.Tank, " +
+                "Venicles.Consumption " +
+                "FROM[Venicles] INNER JOIN[VenicleType] " +
+                "ON Venicles.VeniclesTypeId = VenicleType.VenicleTypeId"
+                ).ToList();
+        }
+    }
+}
