0fb65cf1c20dd3eba37fa5179090cd8cf42feaff
Kirill <kirill.shlemen@gmail.com>
2021-01-21 23:10:24 +0300

    add IVOrderDetails interface

---
+++ Autopark Web Version/Models/Interfaces/IVOrderDetailsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Autopark_Web_Version.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Autopark_Web_Version.Models.Interfaces
+{
+    interface IVOrderDetailsRepository
+    {
+        IEnumerable<VOrderDetails> GetAll();
+    }
+}
