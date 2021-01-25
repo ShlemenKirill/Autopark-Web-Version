a47129410d125a17b5307aeef1071f7d782cdca9
Kirill <kirill.shlemen@gmail.com>
2021-01-25 23:06:02 +0300

    edit Order view, add more simple creating order

--- Autopark Web Version/Controllers/OrdersController.cs
+++ Autopark Web Version/Controllers/OrdersController.cs
@@ -1,32 +1,36 @@
 ﻿using Autopark_Web_Version.Models.Models;
 using Microsoft.AspNetCore.Mvc;
 using Autopark_Web_Version.Models.Interfaces;
+using Autopark_Web_Version.Models;
 
 namespace Autopark_Web_Version.Controllers
 {
     public class OrdersController : Controller
     {
-        IReadOnlyRepository<VOrders> repo;
+        IReadOnlyRepository<VOrders> ordersView;
         IOrdersRepository<Orders> orders;
-        public OrdersController(IReadOnlyRepository<VOrders> r, IOrdersRepository<Orders> ord)
+        IVenicleRepository<Venicles> venicle;
+        public OrdersController(IReadOnlyRepository<VOrders> ordersView, IOrdersRepository<Orders> orders, IVenicleRepository<Venicles> venicle)
         {
-            repo = r;
-            orders = ord;
+            this.ordersView = ordersView;
+            this.orders = orders;
+            this.venicle = venicle;
         }
         public ActionResult Index()
         {
-            return View(repo.GetAll());
+            return View(ordersView.GetAll());
         }
 
         public ActionResult Create()
         {
+            ViewBag.Venicles = venicle.GetAll();
             return View();
         }
 
         [HttpPost]
         public ActionResult Create(Orders order)
         {
-            
+           
             orders.Create(order);
             return RedirectToAction("Index");
         }
